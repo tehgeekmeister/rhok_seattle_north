7e39f09d43ae91c0e6f0f652c2c16bd1d0d202a9
Joe Ferrara <Joe@pop.local>
2010-12-04 19:34:53 -0800

    Modified basic test case to assert on false return value

--- spec/lib/zeke_spec.rb
+++ spec/lib/zeke_spec.rb
@@ -1,7 +1,31 @@
 require 'spec_helper'
 
 describe Zeke do
-  it "We return more than zero results" do
-    assert Zeke.search("denver").toponyms.empty?.should be_false
+
+  # cache search on major U.S. city
+  result = Zeke.search("denver")
+
+  it ( "We return more than zero results") do
+    assert result.toponyms.empty?.should be_false
+  end
+
+  it ( "First place is feature_class of P " ) do
+    assert (result.toponyms[0].feature_class == "P").should be_true
+  end
+
+  it("Empty string passed in as placename") do
+    assert (Zeke.search("").total_results_count == "0").should be_true
+  end
+
+  it("Non text string passed in as placename") do
+    assert (Zeke.search("|").total_results_count == "0").should be_true
+  end
+
+  it("Garbage string passed in as placename") do
+    assert (Zeke.search("jkasdgfkjasdgfkjhasdgfkj").total_results_count == "0").should be_true
+  end
+
+  it("Nearby placename is included in results") do
+    assert result.toponyms.collect { |foo| foo.name }.include?("Town of Denver").should be_true
   end
 end
