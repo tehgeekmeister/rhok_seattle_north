de3c8c554c423c8416635a794b328b4da37df952
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2010-12-05 17:30:44 -0800

    final version for the weekend.  woot.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,24 +1,30 @@
+require 'pp'
 class MainController < ApplicationController
   def index
   end
 
   def refine
     @query = params['q']
-    @results = [
-      {:id => 0, :name => 'springfield',:coordinates => '4QFJ1267',:population => '245000'},
-      {:id => 2, :name => 'springfield',:coordinates => '4QAT3244',:population => '128345'},
-      {:id => 3, :name => 'springfield',:coordinates => '6AAV4548',:population => '359'},
-      {:id => 4, :name => 'springfield',:coordinates => '8MZZ7354',:population => '14000'},
-      {:id => 5, :name => 'springfield',:coordinates => '9XGH4566',:population => '3890000'},
-      {:id => 6, :name => 'springfield',:coordinates => '3EXE3282',:population => '93583'},
-      {:id => 7, :name => 'springfield',:coordinates => '3EZR2138',:population => '237800'}
-      ]
+    @results = Zeke.search(@query).toponyms.collect do |place|
+      puts place.population if place.population and place.population > 0
+      puts place.admin_code_1 if place.admin_code_1
+      puts place.admin_code_2 if place.admin_code_2
+      {
+        :id => "place",
+        :name => place.name,
+        :alternate_names => (place.alternate_names ? place.alternate_names : ""),
+        :value => "#{place.latitude}, #{place.longitude}",
+        :coordinates => "#{place.latitude}, #{place.longitude}"
+      }
+    end
   end
 
   def results
     @radius = params['r']
     @id = params['id']
-    @units = params['units']
-	@results = []
+    # @units = params['units'] todo this should accept kilometers or miles in the future.
+    lat = @id.split[0][0..-2].to_i # these will return 0 if these are non-string values.  might wanna display a warning in that case?
+    lng = @id.split[1][0..-2].to_i
+    @results = Zeke.cities_in_radius(@radius, lat, lng).sort_by {|city| city["population"].to_i}.reverse
   end
 end
